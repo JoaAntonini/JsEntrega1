17a38af3944f81d94e1dbf67b85f0f69c5132aa1
Johanna Antonini <antonini.johanna@gmail.com>
2022-05-09 12:30:12 -0300

    1° entrega obligatoria

--- script1.js
+++ script1.js
@@ -1,8 +1,5 @@
 
-//Defino variables
-// IngresoMinimo = 20000; y antiguedadMinima = 3; 
-
-
+//Defino si por ingreso y antiguedad califica y su monto maximo.
 
 let IngresoNetoFamiliar;
 let AntiguedadLaboral;
@@ -12,18 +9,21 @@ for(let i=1; i<=10; i++){
     AntiguedadLaboral = parseInt(prompt("Antiguedad Laboral"));
     if(IngresoNetoFamiliar < 20000 && AntiguedadLaboral<3){
         alert("No califica para esta linea");
-        break;
+        break; 
     }
-    else if(IngresoNetoFamiliar >=20000 && IngresoNetoFamiliar < 300000 && AntiguedadLaboral>=3){
-        alert("El monto maximo de tu crédito es de $ "+  ((IngresoNetoFamiliar * 0.35) * 20));
-        break;
-    }
-    else if(IngresoNetoFamiliar >=300000 && IngresoNetoFamiliar < 1000000 && AntiguedadLaboral>=3){
-        alert("Por tu ingreso podes acceder al credito plus por $ "+ ( ((IngresoNetoFamiliar * 0.35) * 20) * 1.2));
+    else if(IngresoNetoFamiliar >=20000 && IngresoNetoFamiliar < 1000000 && AntiguedadLaboral>=3){
+         let capital=prompt("Introduzca el monto del prestamo a solicitar \n Monto minimo 2000000\n Monto máximo 10000000");
+         let Interes =  parseInt(0.2);
+         let linea=prompt("Indique la linea que le interesa \n Construcción\n Compra Vivienda\n Construcción+Terreno"); //segun la linea va a ser el interes
+         let anos=prompt("Lo va a pagar en 10 o 20 años");
+         function calcular() {
+            let cuota =  (capital / anos / 12 ) * ((1 + Interes))
+             alert ("Para un prestamo de "+capital+ " a "+ (anos*12)+ " meses y una tasa del 20%, su cuota mensual sera de " + parseFloat(cuota)) ;
+            document.write("<h3> La cuota Mensual sera de " +parseFloat(cuota) + "</h3>" )
+            }
+        calcular(); 
         break;
     }
     else alert("Comunicate con un representate para revisar tu casa");
-    break
+    break; 
 }
-
-
